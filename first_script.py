9086e8e29488a57ef63831e839f91a2c7e7f4cd5
ANF1S <harcos9112@gmail.com>
2025-06-12 09:32:20 +0200

    script modified

--- first_script.py
+++ first_script.py
@@ -4,7 +4,6 @@ from names import nameit
 
 root = Tk()
 root.title('Code Pablo')
-#root.iconbitmap("home/snap/snap-store/518/.local/share/icons/ubuntu-mono-dark/actions/16/package-supported.ico")
 root.geometry("500x350")
 
 greet = nameit("LoL")
